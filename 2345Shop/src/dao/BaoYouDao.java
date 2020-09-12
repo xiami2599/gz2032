8899110aa7009bdcd7f48be4e4fc3271803279dc
xm <1591565655@qq.com>
2020-09-12 11:15:20 +0800

    修改包邮接口

--- 2345Shop/src/dao/BaoYouDao.java
+++ 2345Shop/src/dao/BaoYouDao.java
@@ -2,8 +2,11 @@ package dao;
 
 import dao.entity.BaoYou;
 import dto.Page;
-
+/*
+ * 包邮接口
+ */
 public interface BaoYouDao {
+	
 	
 	public Page<BaoYou> getOnePage(int currentPage,int pageSize) ;
 	public int save(BaoYou ddq);
